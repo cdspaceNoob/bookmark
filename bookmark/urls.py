60a7b0fbe1217cc61e53c3ce0d102d35a0969db4
cdspaceNoob <pokapong@naver.com>
2021-11-05 15:45:04 +0900

    to p112 line7

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -4,7 +4,11 @@
 from django.urls import path
 from .views import BookmarkListView, BookmarkCreateView
 
+# 북마크 확인 기능 뷰에 URL을 연결하기 위한 임포트
+from .views import *
+
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),
     path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -8,6 +8,9 @@ from .models import Bookmark
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
+# 북마크 확인 기능 구현을 위한 임포트
+from django.views.generic.detail import DetailView
+
 # Create your views here.
 # 목록을 보여주는 뷰 만들기
 class BookmarkListView(ListView):
@@ -19,3 +22,7 @@ class BookmarkCreateView(CreateView):
     fields = ['site_name', 'url']
     success_url = reverse_lazy('list')
     template_name_suffix = '_create'
+
+# 북마크 확인 기능 구현을 위한 코드
+class BookmarkDetailView(DetailView):
+    model = Bookmark
